77555f3b7db5fb8b63cad420f8db10288fccaeee
Joseph Collard <jcollard@users.noreply.github.com>
2022-01-13 03:39:31 -0800

    chore: Initialize project.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace jcollard_DataEncodingInCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
